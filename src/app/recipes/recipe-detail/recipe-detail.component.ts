cbc59e0886b4ede5d7762c89dc65bde0cb1f0e2b
Calin Rafa <calin.rafa@evozon.com>
2019-10-22 14:00:23 +0300

    44/45/46 - created the components

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-detail',
+  templateUrl: './recipes-detail.component.html',
+  styleUrls: ['./recipes-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

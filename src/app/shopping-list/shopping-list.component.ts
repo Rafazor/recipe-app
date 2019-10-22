65123d37b71df8deb93435e6674567280499befc
Calin Rafa <calin.rafa@evozon.com>
2019-10-22 17:12:48 +0300

    Added more components boilerplate

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Mere', 4),
+    new Ingredient('Pere', 2),
+    new Ingredient('Pita', 3),
+    new Ingredient('cas', 4)
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }

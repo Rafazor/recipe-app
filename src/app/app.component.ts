c31861c78ebd1ecfbc346f5f7e658702e1d597d8
Calin Rafa <calin.rafa@evozon.com>
2019-10-24 13:21:15 +0300

    Added simple navigation between recipes and shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+  route = 'recipes';
+
+  changedRoute(route: string) {
+    this.route = route;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() route = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  changeRoute(route: string): void {
+    if (route === 'recipes') {
+      this.route.emit('recipes');
+    } else {
+      this.route.emit('shopping');
+    }
+  }
 }
